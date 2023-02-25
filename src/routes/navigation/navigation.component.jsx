54194dec321cf741cd058d3d0aa4011a0109ad44
David Ologungbara <davido@DavidOlgbaraMBP.lan>
2023-02-25 11:32:00 +0100

    sign out function

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,21 @@
-import { Fragment } from 'react';
+import { Fragment,useContext } from 'react';
 import {Outlet,Link} from 'react-router-dom'
+
+import { UserContext } from '../../context/user-context';
+import { signOutUser } from '../../utilities/firebase/firebase.utils';
 
 import './navigation.styles.scss';
 
 
 const Navigation = () => {
+    const {currentUser, setCurrentUser} = useContext(UserContext);
+
+
+    const handleSignOut = () => {
+        signOutUser();
+        setCurrentUser(null)
+    }
+
     return (
         <Fragment>
             <div className='navigation'>
@@ -18,9 +29,18 @@ const Navigation = () => {
                     <Link className='nav-link' to='/shop'>
                     SHOP
                     </Link>
-                    <Link className='nav-link' to='/auth'>
-                    SIGN IN
-                    </Link>
+                    {
+                        currentUser 
+                        ?
+                        <span className='nav-link' onClick={handleSignOut}>SIGN OUT</span>
+                        :
+                        (
+                            <Link className='nav-link' to='/auth'>
+                            SIGN IN
+                            </Link>
+                        ) 
+                    }
+                   
                 </div>
             </div>
             <Outlet />
